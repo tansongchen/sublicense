c960884e2c4e717a218d9edcf73b490385ae4d47
Songchen Tan <i@tansongchen.com>
2024-11-11 21:14:21 -0500

    Update

--- App.tsx
+++ App.tsx
@@ -1,32 +1,40 @@
-import { useState } from 'react'
 import { Stream } from "@cloudflare/stream-react";
-import './App.css'
+import React from "react";
 
 function App() {
-  const video = 'ed186a2d2c850220726e752ac2f01ef1';
+  const video = "9fff09d6fdf59b7ff66b8193729ce401";
   return (
     <>
-      <h1>🏠 MIT 校内 Studio 公寓暑假转租</h1>
+      <h1>🏠 MIT 校内 1B1B 公寓转租</h1>
       <p className="read-the-docs">
-📆 6.1 ~ 9.5 期间可租<br/>
-💰 租金 $1500/月（包水电）<br/>
-✅ 租金和租期均可协商
+        📆 租期：2024.12.25 ~ 2025.2.5
+        <br />
+        💰 租金：$2000/月（包水电）
+        <br />✅ 租金和租期均可协商
       </p>
       <ul>
-        <li>地址：70 Pacific Street (Sidney-Pacific)</li>
-        <li>周边：步行 10 分钟可达 MIT 主校区、地铁红线、H-Mart 和 Target 等，15分钟到 Trader Joe’s 和 Whole Foods</li>
-        <li>公寓设施：健身房、洗衣机烘干机、活动室、音乐室、每层楼有公共厨房和公共学习室</li>
-        <li>房间设施：家具齐全（床架床垫、衣柜、书桌书架、餐桌、冰箱、烤箱和微波炉等厨具），可拎包入住</li>
+        <li>地址：269 Vassar Street (Graduate Junction)</li>
+        <li>
+          周边：步行 10 分钟可达 MIT 主校区、地铁红线、H-Mart 和 Target 等，15
+          分钟到 Trader Joe’s 和 Whole Foods 等
+        </li>
+        <li>
+          房间设施：客厅有沙发、餐桌、电视柜，厨房有冰箱、烤箱、洗碗机、微波炉等，卧室有
+          queen size (1.5 m) 床、衣柜、书桌椅等
+        </li>
+        <li>楼内设施：洗衣机、烘干机、健身房、活动室、自习室</li>
       </ul>
-      <p>
-        点击下方查看房间实景视频
-      </p>
-      <Stream controls src={video} />
-      <p>
-        有意者请加微信 tansongchen 私聊或发邮件至 i@tansongchen.com 联系～
-      </p>
+      <p>有意者请加微信 tansongchen 私聊或发邮件至 i@tansongchen.com 联系～</p>
+      <p>点击下方查看房间实景视频和平面图</p>
+      <div style={{ alignSelf: "stretch" }}>
+        <Stream controls src={video} />
+      </div>
+      <img
+        src="https://images.tansongchen.com/1731374370.png"
+        alt="房间平面图"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
